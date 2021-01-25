835dc318db06ab2cd74cd971555be41ec7ee84bf
Naveen Kumar Srinivas <adith.naveen@gmail.com>
2021-01-25 13:00:17 +0530

    added files after in day 1 session 1

---
+++ workspace/html-woks/script/script.js
@@ -0,0 +1,12 @@
+/// only functions go here 
+
+function checkVote() {
+     
+    var age = prompt("Enter Your Age")
+    alert("Hey Your age is " + age)
+
+    if(age > 18)
+        alert("you can vote")
+    else 
+        alert("you cannot vote ")
+}
