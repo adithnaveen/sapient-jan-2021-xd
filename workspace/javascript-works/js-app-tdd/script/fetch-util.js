42bfd9915b9277557843cf799827dbb5f1353df9
Naveen Kumar Srinivas <adith.naveen@gmail.com>
2021-02-17 16:21:36 +0530

    added files for fetch & images

---
+++ workspace/javascript-works/js-app-tdd/script/fetch-util.js
@@ -0,0 +1,13 @@
+exports.myFetch= (url)  => {
+    let jsonPosts ; 
+    fetch().then().then() 
+
+    return jsonPosts; 
+}
+
+exports.myFetch= (url)  => {
+    let jsonPosts ; 
+    fetch().then().then() 
+
+    return jsonPosts; 
+}
