7b8e2dda08ddd113276e2a97c2d31d047937c01b
Naveen Kumar Srinivas <adith.naveen@gmail.com>
2021-03-01 13:33:04 +0530

    mongodb client connect -services

--- workspace/nodejs-works/http/07-express-contact-service-async.js
+++ workspace/nodejs-works/http/07-express-contact-service-async.js
@@ -8,6 +8,14 @@ const app = express();
 
 app.use(bodyParser.json()); 
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*"); 
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "*");
+    next(); 
+})
+
+
 // get 1 record
 // version 1.0 
  app.get("/api/contacts/:id", (req, res) => {
---
+++ workspace/nodejs-works/http/services/contact-service-mongodb.js
@@ -0,0 +1,81 @@
+const mc = require("mongodb").MongoClient; 
+const ObjectId = require("mongodb").ObjectId; 
+
+
+const url = "mongodb://localhost";
+const dbName = "sap-db";
+const collection = "contacts";
+
+// helper function thats gives the mongo client, with a callback function 
+function getContactsCollection(callback) {
+    // parameters for the callback 
+    // 1. err -> if any error exits, error is sent 
+    // 2. client -> reference to mongo-client, we can use it to close the connection
+    // 3. contacts -> referenc to the contact collection in sap-db 
+    mc.connect(url,(err, client) => {
+        if(err) {
+            callback(err); 
+            return; 
+        }
+        const db = client.db(dbName); 
+        const contactsCollections = db.collection(collection); 
+        callback(null, client, contactsCollections); // 1st arg is null as there is no error here 
+
+    })
+} 
+
+
+
+class ContactService  {
+
+  addContact(contact){
+ 
+    return new Promise((resolve, reject) => {
+        if(!contact || typeof contact != 'object') {
+            let err ={}; 
+            err.code = 1002; 
+            err.message = "contact was not supplied or was not an object"; 
+            reject(err); 
+            return ;
+        }
+
+         // check all the fields are passed
+         const requiredFields = ['name', 'email', 'phone', 'city'];
+         const missingFields = [];
+ 
+         requiredFields.forEach(function(field){
+             if(!(field in contact)){
+                 missingFields.push(field)
+             }
+         });
+         if(missingFields.length!==0){
+             let err ={}; 
+             err.code = 1003; 
+             err.message = "required fields missing" + missingFields.join(); 
+             callback(err); 
+             return ;
+         }
+
+        getContactsCollection((err, client, contacts) => {
+            if(err) {
+                reject(err); 
+                return ; 
+            }
+            contacts.insertOne(contact, (err, doc) => {
+                if(err) {
+                    reject(err); 
+                }else {
+                    resolve(doc.insertedId); 
+                }
+                client.close();
+            })
+
+
+
+    })
+
+
+
+
+ 
+}
