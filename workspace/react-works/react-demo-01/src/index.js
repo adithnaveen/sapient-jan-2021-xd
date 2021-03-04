13ce4aa4d2d900e133d860413f645d791e6ba52c
Naveen Kumar Srinivas <adith.naveen@gmail.com>
2021-03-04 11:50:30 +0530

    added contacts card app

--- workspace/react-works/react-demo-01/src/index.js
+++ workspace/react-works/react-demo-01/src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 // import App from './App-02';
 // import App from './App-03';
 // import App from './apps/App-Event-04'
-
-import App from './apps/App-Product'
+// import App from './apps/App-Product'
+// import AppLifeCycle from './apps/App-Life-Cycle-06'
+import ContactApp from './apps/Contact-App'
 
 ReactDOM.render(
-    <App />,document.getElementById('root')
+    <ContactApp />,document.getElementById('root')
 );
 
-export default App;
+export default ContactApp;
