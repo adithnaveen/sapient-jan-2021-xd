d9816909eb2046066a35aef36d8eaea22a77023b
Naveen Kumar Srinivas <adith.naveen@gmail.com>
2021-03-08 12:05:01 +0530

    after counter app

---
+++ workspace/react-works/react-tdd/src/Counter.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+const Counter = () => {
+    const [count, setCount] = useState(0);
+    return (
+        <div>
+            <h1>Counter App</h1>
+            <h3 id="count-value">{count}</h3>
+
+            <button id="incr-btn"
+                onClick={() => {
+                    setCount(count + 1)
+                }}
+            >Increment</button>
+
+            <button id="decr-btn"
+                onClick={() => {
+                    setCount(count - 1)
+                }}
+            >Decrement</button>
+
+
+        </div>
+    );
+}
+export default Counter;
---
+++ workspace/react-works/react-tdd/src/tests/App.test.js
@@ -0,0 +1,52 @@
+import {render} from '@testing-library/react'
+
+import {shallow, configure} from 'enzyme'
+// import App from '../App'; 
+import Counter from '../Counter'; 
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+configure({adapter : new Adapter()})
+
+
+describe('My Test Counter Application', () => {
+    
+    test.skip('look for the text called Counter App  ', () => {
+        const h1Element = render(<App />).getByText("Counter App"); 
+        expect(h1Element).toBeInTheDocument();
+    })
+
+    let wrapper ; 
+    beforeEach(() => {
+        wrapper = shallow(<Counter />); 
+    })
+
+    
+
+    test("looking for the text called Counter App", () => {
+        const expectedText = "Counter App"; 
+        let actualText = wrapper.find("h1").text(); 
+        expect(actualText).toContain(expectedText); 
+        
+        //  console.log(wrapper.debug());
+    })
+
+    test('to validate count value ', () => {
+        expect(wrapper.find("#count-value").text()).toBe("0"); 
+    })
+    
+    test("to increment the value of count", () => {
+        wrapper.find("#incr-btn").simulate("Click"); 
+        expect(wrapper.find("#count-value").text()).toBe("1"); 
+    })
+
+    test("To increment and decrement the value of count", () => {
+        // initial value will be 0 
+        wrapper.find("#incr-btn").simulate("click");
+        expect(wrapper.find("#count-value").text()).toBe("1");
+        
+        wrapper.find("#decr-btn").simulate("click"); 
+        expect(wrapper.find("#count-value").text()).toBe("0");
+    })
+
+
+})
