0a722396993d904749c96471060fe3c1dab23396
Naveen Kumar Srinivas <adith.naveen@gmail.com>
2021-03-10 11:57:09 +0530

    building react redux

--- workspace/react-works/redux-works/src/index.js
+++ workspace/react-works/redux-works/src/index.js
@@ -5,10 +5,19 @@ import 'bootstrap/dist/css/bootstrap.css'
 import ContactsList from './components/ContactList'
 import ContactForm from './components/ContactForm'; 
 
+import {createStore} from 'redux'; 
+import rootReducer from './reducers/root-reducer';
+// dont use this in production
+import {composeWithDevTools} from 'redux-devtools-extension'; 
+import {Provider} from 'react-redux';
+const store = createStore(rootReducer, composeWithDevTools());
+
+
 class App extends Component {
     render() {
         return (
-            <div className="container">
+           <Provider store={store}>
+                <div className="container">
                 <h2 className="alert alert-info">React Redux Application </h2>
                 <div className="row">
                     <div className="col-md-5">
@@ -20,6 +29,7 @@ class App extends Component {
                 </div>
 
             </div>
+           </Provider>
         );
     }
 }
